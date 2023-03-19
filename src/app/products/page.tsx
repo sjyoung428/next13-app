77360b28d18310a5f0b6337a15ad24ebe45da803
sjyoung428 <sjy8267@naver.com>
2023-03-20 00:04:46 +0900

    Next Image Component

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,17 +1,18 @@
 import { getProducts } from "@/service/products";
 import Link from "next/link";
 import MeowArticle from "@/components/MeowArticle";
+import ClothesImage from "../../../public/images/clothes.jpg";
+import Image from "next/image";
 
 // export const revalidate = 3;
 
 export default async function Products() {
-  throw new Error();
-
   const products = await getProducts();
 
   return (
     <>
       <h1>Products</h1>
+      <Image src={ClothesImage} alt="옷" />
       <ul>
         {products.map((product) => (
           <li key={product.id}>
