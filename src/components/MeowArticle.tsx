1a07fd22fd6afaae10751760c7b0fbf4c64f3e89
sjyoung428 <sjy8267@naver.com>
2023-03-19 17:55:11 +0900

    CSR

---
+++ src/components/MeowArticle.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import styles from "./MeowArticle.module.css";
+
+const MeowArticle = () => {
+  const [text, setText] = useState("Loading...");
+
+  useEffect(() => {
+    fetch("https://meowfacts.herokuapp.com/")
+      .then((res) => res.json())
+      .then((data) => setText(data.data[0]));
+  }, []);
+
+  return <article className={styles.article}>{text}</article>;
+};
+
+export default MeowArticle;
