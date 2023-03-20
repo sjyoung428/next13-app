83df4e3be75163e7b94386cd45f10fb6966baefe
sjyoung428 <sjy8267@naver.com>
2023-03-20 22:19:07 +0900

    redirect

--- next.config.js
+++ next.config.js
@@ -11,6 +11,20 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/products/deleted_forever",
+        destination: "/products",
+        permanent: true,
+      },
+      {
+        source: "/products/deleted_temp",
+        destination: "/products",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
